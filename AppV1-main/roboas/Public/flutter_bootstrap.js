55447286ffa76fa1a76a7ab5bd01e486d60846b1
qunatumstuff <dominicalassir@gmail.com>
2026-05-29 13:15:38 +0800

    baka

--- AppV1-main/roboas/Public/flutter_bootstrap.js
+++ AppV1-main/roboas/Public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3031046087" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2470973208" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
